90f9d1de11e2e91066616f6204a58b35ac6cf83a
ZitaTheSpacegirl <zita.brunnhuber@gmail.com>
2013-05-31 16:53:59 +0200

    fixed logo url

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <div id="loginForm">
-	<div id="logo"><img src="images/logo_120x40.png" alt="Programm: Felix Online Systems"/></div>
+	<div id="logo"><img src="img/logo_120x40.png" alt="Programm: Felix Online Systems"/></div>
 	
 	<form>
             <fieldset>
