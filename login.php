82a3566d700d00194260e5fa48809adaaa45b460
ZitaTheSpacegirl <zita.brunnhuber@gmail.com>
2013-05-31 17:07:45 +0200

    reverted my changes

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 	
 	<form>
             <fieldset>
-                    <label>Benutzername123:</label>
+                    <label>Benutzername:</label>
                     <input type="text">
             </fieldset>
             <fieldset>
