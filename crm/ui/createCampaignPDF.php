21c3f9aa04566e43a09ee076eed4ce0b18c894e4
oidawtf <ILoveBJC@hotmail.com>
2013-06-17 21:47:47 +0200

    .

--- crm/ui/createCampaignPDF.php
+++ crm/ui/createCampaignPDF.php
@@ -2,7 +2,7 @@
 
 require_once '../../shared/authenticationController.php';
 require_once '../includes.php';
-require '../../shared/fpdf/fpdf.php';
+require_once '../../shared/fpdf/fpdf.php';
 
 authenticationController::checkAuthentication();
 
@@ -18,30 +18,30 @@ $articles = controller::getSelectedArticles($campaignId);
 class PDF extends FPDF
 {
 // Page header
-function Header()
-{
-    // Logo
-    $this->Image('../../img/logo_120x40_transparent.png',10,6,30);
-    // Arial bold 15
-    $this->SetFont('Arial','B',15);
-    // Move to the right
-    $this->Cell(80);
-    // Title
-    $this->Cell(30,10,'Title',1,0,'C');
-    // Line break
-    $this->Ln(20);
-}
+    function Header()
+    {
+        // Logo
+        $this->Image('../../img/logo_120x40_transparent.png',10,6,30);
+        // Arial bold 15
+        $this->SetFont('Arial','B',15);
+        // Move to the right
+        $this->Cell(80);
+        // Title
+        $this->Cell(30,10,'Title',1,0,'C');
+        // Line break
+        $this->Ln(20);
+    }
 
-// Page footer
-function Footer()
-{
-    // Position at 1.5 cm from bottom
-    $this->SetY(-15);
-    // Arial italic 8
-    $this->SetFont('Arial','I',8);
-    // Page number
-    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
-}
+    // Page footer
+    function Footer()
+    {
+        // Position at 1.5 cm from bottom
+        $this->SetY(-15);
+        // Arial italic 8
+        $this->SetFont('Arial','I',8);
+        // Page number
+        $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+    }
 }
 
 // Instanciation of inherited class
